6cce69d42b10790551789f0ba776df087f960bce
Magnus Östgren <ninjalostinsnow@gmail.com>
2018-12-19 22:18:42 +0100

    Kind of working communication. Waits for all data, but keeps sending
    Signed-off-by: Magnus Östgren <ninjalostinsnow@gmail.com>

--- bitonicSort/main.vhd
+++ bitonicSort/main.vhd
@@ -57,69 +57,61 @@ architecture Flow of main is
 	end component;
 	
 	signal rst: std_logic;
-	
-	type state_t is (R_s, W_s);
-	
-	signal state : state_t := R_s;
-	
-	
-	signal input, output: list(2 ** size - 1 downto 0);
-	signal count: unsigned(size - 1 downto 0) := (others => '0');
-	signal t_data, r_data: unsigned(7 downto 0);
-	signal r_ready, t_ready, t_done, t_active : std_logic;
+	signal tick: std_logic;
+	signal input, output, tmp: list(2 ** size - 1 downto 0);
+	signal count: unsigned(size downto 0) := (others => '0');
+	signal r_ready, t_ready, t_done, t_active : std_logic := '0';
+	signal byteIn, byteOut: unsigned(7 downto 0);
 	
 begin
 	
 	rst <= not rst_n;
 	
 	sort: bitonicSort generic map(logN => size)
-							   port map(unsorted => input, sorted => output);
+							   port map(unsorted => input, sorted => tmp);
 	
 	
 	receiver: UART_RX generic map (g_CLKS_PER_BIT => 5208)
-								port map (i_Clk => clk, i_RX_Serial => rx, o_RX_DV => r_ready, unsigned(o_RX_Byte) => r_data);
+								port map (i_Clk => clk, i_RX_Serial => rx, o_RX_DV => r_ready, unsigned(o_RX_Byte) => input(0));
 	
 	
 	transmitter: UART_TX generic map (g_CLKS_PER_BIT => 5208)
-									port map (i_Clk => clk, i_TX_DV => t_ready, i_TX_Byte => std_logic_vector(t_data), o_TX_Active => t_active, o_TX_Serial => tx, o_TX_Done => t_done);
+									port map (i_Clk => clk, i_TX_DV => t_ready, i_TX_Byte => std_logic_vector(byteOut), o_TX_Active => t_active, o_TX_Serial => tx, o_TX_Done => t_done);
 	
 	
-	process(rst, r_ready, t_done, count, state)
+	
+	byteOut <= output(to_integer(count(size - 1 downto 0)));
+	
+	
+	tick <= r_ready or t_done;
+	
+	counter: process(tick, count, rst, tmp)
 	begin
 		if (rst = '1') then
-			state <= R_s;
 			count <= (others => '0');
+		elsif (tick'EVENT and tick = '1') then
+			count <= count + 1;
+			if count(size) = '1' and t_active = '0' then
+				t_ready <= '1';
+			end if;
 		end if;
+		if count(size - 1 downto 0) = to_unsigned(2 ** size - 1, size) then
+			output <= tmp;
+		end if;
+	end process counter;
 		
-		case state is
-			when R_s => --Read state
-				if r_ready'EVENT and r_ready = '1' then
-					input(to_integer(count)) <= r_data;
-					count <= count + 1;
-				end if;
-				
-				if count = to_unsigned(2 ** size - 1, size) then
-					state <= W_s;
-					count <= (others => '0');
-					t_data <= (others => '0');
-					t_ready <= '1';
-				end if;
-				
-			when W_s => --Write state
-				if t_done'EVENT and t_done = '1' then
-					t_data <= output(to_integer(count));
-					count <= count + 1;
-					t_ready <= '1';
-				end if;
-				
-				if count = to_unsigned(2 ** size - 1, size) then
-					state <= R_s;
-					count <= (others => '0');
-				end if;
-				
-		end case;
-		
-	end process;
+	inputShift: for I in 1 to 2 ** size - 1 generate
+		process(r_ready)
+		begin
+			if (r_ready'EVENT and r_ready = '1') then
+				input(I) <= input(I-1);
+			end if;
+		end process;
+	end generate inputShift;
+	
+	
+	
+	
 	
 	
 
