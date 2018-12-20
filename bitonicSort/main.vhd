d5061f7ffd01d5aba65c05861933c246de20d955
Magnus Östgren <ninjalostinsnow@gmail.com>
2018-12-20 21:38:19 +0100

    I simplified it a lot, but now it work. At least in the simple form.
    Signed-off-by: Magnus Östgren <ninjalostinsnow@gmail.com>

--- bitonicSort/main.vhd
+++ bitonicSort/main.vhd
@@ -10,7 +10,6 @@ entity main is
 		clk: in STD_LOGIC;
 		rst_n: in STD_LOGIC;
 		tx: out STD_LOGIC;
-		rx: in  STD_LOGIC;
 		led: out unsigned(7 downto 0)
 	);
 end main;
@@ -18,7 +17,8 @@ end main;
 architecture Flow of main is
 
 
-	constant size: integer := 3;
+	constant size: integer := 5;
+	constant top: integer := 28;
 
 	component bitonicSort is
 		generic(
@@ -31,110 +31,83 @@ architecture Flow of main is
 	end component;
 	
 	
-	component UART_RX is
+	
+	component serial_tx is
 		generic (
-			g_CLKS_PER_BIT : integer := 115     -- Needs to be set correctly
+			CLK_PER_BIT : integer := 115     -- Needs to be set correctly
 		);
 		port (
-			i_Clk       : in  std_logic;
-			i_RX_Serial : in  std_logic;
-			o_RX_DV     : out std_logic;
-			o_RX_Byte   : out std_logic_vector(7 downto 0)
+			clk: in  std_logic;
+			rst: in std_logic;
+ 			tx: out std_logic;
+			b: in std_logic;
+			busy: out std_logic;
+			data: in std_logic_vector(7 downto 0);
+			new_data: in std_logic
 		);
 	end component;
 	
-	component UART_TX is
-		generic (
-			g_CLKS_PER_BIT : integer := 115     -- Needs to be set correctly
-		);
-		port (
-			i_Clk       : in  std_logic;
-			i_TX_DV     : in  std_logic;
-			i_TX_Byte   : in  std_logic_vector(7 downto 0);
-			o_TX_Active : out std_logic;
-			o_TX_Serial : out std_logic;
-			o_TX_Done   : out std_logic
-		);
-	end component;
-	
-	signal rst: std_logic;
+	signal rst, tick: std_logic;
 	signal output: list(2 ** size - 1 downto 0);
-	signal input: list(2 ** size downto 0);
-	signal countIn: unsigned(size downto 0);
-	signal countOut: unsigned(20 downto 0) := (others => '0');
-	signal r_ready, t_ready, t_done, t_active: std_logic := '0';
+	signal input: list(2 ** size - 1 downto 0);
+	signal countOut: unsigned(top downto 0) := (others => '0');
+	signal t_ready, t_done, t_active: std_logic;
 	signal byteOut: unsigned(7 downto 0);
-	
-	type state is (R, W);
-	signal mode: state := R;
+	signal w : std_logic := '1';
 begin
 	
 	rst <= not rst_n;
 	
-	led(size downto 0) <= countIn;
-	led(6 downto size + 1) <= (others => '0');
-	led(7) <= t_active;
+--	led(size downto 0) <= countOut(top downto top - size);
+--	led(5 downto size + 1) <= (others => '0');
+--	led(6) <= t_ready;
+--	led(7) <= t_active;
+	
+	
+	
+	
 	
 	sort: bitonicSort generic map(logN => size)
-							   port map(unsorted => input(2 ** size downto 1), sorted => output);
+							   port map(unsorted => input(2 ** size - 1 downto 0), sorted => output);
 	
 	
-	receiver: UART_RX generic map (g_CLKS_PER_BIT => 5208)
-								port map (i_Clk => clk, i_RX_Serial => rx, o_RX_DV => r_ready, unsigned(o_RX_Byte) => input(0));
+	setup: for I in 0 to 2 ** size - 1 generate
+		input(I) <= to_unsigned((2 ** size - I) mod 7, 8);
+	end generate setup;
 	
 	
-	transmitter: UART_TX generic map (g_CLKS_PER_BIT => 5208)
-									port map (i_Clk => clk, i_TX_DV => t_ready, i_TX_Byte => std_logic_vector(byteOut), o_TX_Active => t_active, o_TX_Serial => tx, o_TX_Done => t_done);
+	transmitter: serial_tx generic map (CLK_PER_BIT => 5208)
+									port map (clk => clk, rst => '0', tx => tx, b => '0', busy => t_active, data => std_logic_vector(byteOut), new_data => t_ready and w);
 	
 	
 
-	byteOut <= output(to_integer(countOut(20 downto 20 - size)));
+	byteOut <= output(to_integer(countOut(top downto top - size)));
 	
-	t_ready <= countOut(20 - size - 1);
+	led <= byteOut;
 	
-	counter: process(r_ready, clk, rst, mode, countIn, countOut)
+	tick <= countOut(top - size - 1);
+	process(tick, w, t_active)
 	begin
-		if (rst = '1') then
-			countIn <= (others => '0');
-			countOut <= (others => '0');
-			mode <= R;
-		else 
-			case mode is
-				when R =>
-				if (r_ready'EVENT and r_ready = '1') then
-					countIn <= countIn + 1;
-				end if;
-				if countIn = to_unsigned(2**size- 1, size) then
-					mode <= W;
-					countIn <= (others => '0');
-				end if;
-				
-				when W =>
-				if clk'EVENT and clk = '1' then
-					if t_active = '0' then
-						countOut <= countOut + 1;
-					end if;
-					if countOut(20 downto 20 - size) = to_unsigned(2**size- 1, size) then
-						mode <= R;
-						countOut <= (others => '0');
-					end if;
-				end if;
-			end case;
+		if t_active = '1' then
+			t_ready <= '0';
+		elsif tick'EVENT and tick = '1' then
+			t_ready <= '1';
 		end if;
-	end process counter;
+	end process;
 	
-		
-		
-	inputShift: for I in 1 to 2 ** size generate
-		process(r_ready)
-		begin
-			if (r_ready'EVENT and r_ready = '1') then
-				input(I) <= input(I-1);
-			end if;
-		end process;
-	end generate inputShift;
+
 	
-	
+	count: process (clk, countOut, rst)
+	begin
+		if rst = '1' then
+			countOut <= (others => '0');
+			w <= '1';
+		elsif countOut(top downto top - size) = to_unsigned(2 ** size - 1, size) then
+			w <= '0';
+		elsif clk'EVENT and clk = '1' then
+			countOut <= countOut + 1;
+		end if;
+	end process count;
 	
 	
 	
