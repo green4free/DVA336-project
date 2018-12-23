e0e2430492e0605af6a6d69a9d8f21a80fe6a210
Magnus Östgren <ninjalostinsnow@gmail.com>
2018-12-23 12:53:53 +0100

    Removed unused files, and fixed code to do both input and output. Right now semi-working state, some timing issues.
    Signed-off-by: Magnus Östgren <ninjalostinsnow@gmail.com>

--- bitonicSort/main.vhd
+++ bitonicSort/main.vhd
@@ -9,6 +9,7 @@ entity main is
 	port (
 		clk: in STD_LOGIC;
 		rst_n: in STD_LOGIC;
+		rx: in STD_LOGIC;
 		tx: out STD_LOGIC;
 		led: out unsigned(7 downto 0)
 	);
@@ -47,13 +48,29 @@ architecture Flow of main is
 		);
 	end component;
 	
+	
+	component serial_rx is
+		generic (
+			CLK_PER_BIT : integer := 115     -- Needs to be set correctly
+		);
+		port (
+			clk: in  std_logic;
+			rst: in std_logic;
+ 			rx: in std_logic;
+			data: out std_logic_vector(7 downto 0);
+			new_data: out std_logic
+		);
+	end component;
+		
+	
 	signal rst, tick: std_logic;
 	signal output: list(2 ** size - 1 downto 0);
 	signal input: list(2 ** size - 1 downto 0);
 	signal countOut: unsigned(top downto 0) := (others => '0');
-	signal t_ready, t_done, t_active: std_logic;
-	signal byteOut: unsigned(7 downto 0);
-	signal w : std_logic := '1';
+	signal countIn: unsigned(size - 1 downto 0) := (others => '0');
+	signal t_ready, t_done, t_active, r_done: std_logic;
+	signal byteOut, byteIn: unsigned(7 downto 0);
+	signal w : std_logic := '0';
 begin
 	
 	rst <= not rst_n;
@@ -71,21 +88,22 @@ begin
 							   port map(unsorted => input(2 ** size - 1 downto 0), sorted => output);
 	
 	
-	setup: for I in 0 to 2 ** size - 1 generate
-		input(I) <= to_unsigned((2 ** size - I) mod 7, 8);
-	end generate setup;
+	receiver: serial_rx generic map (CLK_PER_BIT => 5208)
+							  port map (clk => clk, rst => rst, rx => rx, unsigned(data) => byteIn, new_data => r_done);
 	
 	
 	transmitter: serial_tx generic map (CLK_PER_BIT => 5208)
-									port map (clk => clk, rst => '0', tx => tx, b => '0', busy => t_active, data => std_logic_vector(byteOut), new_data => t_ready and w);
+								  port map (clk => clk, rst => rst, tx => tx, b => '0', busy => t_active, data => std_logic_vector(byteOut), new_data => t_ready and w);
 	
 	
-
-	byteOut <= output(to_integer(countOut(top downto top - size)));
+	
+	byteOut <= output(to_integer(countOut(top downto top - size - 1)));
+	
+	input(to_integer(countIn)) <= byteIn;
 	
 	led <= byteOut;
 	
-	tick <= countOut(top - size - 1);
+	tick <= countOut(top - size - 2);
 	process(tick, w, t_active)
 	begin
 		if t_active = '1' then
@@ -97,15 +115,32 @@ begin
 	
 
 	
-	count: process (clk, countOut, rst)
+	count: process (clk, countOut, countIn, r_done, rst)
 	begin
 		if rst = '1' then
 			countOut <= (others => '0');
-			w <= '1';
-		elsif countOut(top downto top - size) = to_unsigned(2 ** size - 1, size) then
+			countIn <= (others => '0');
 			w <= '0';
-		elsif clk'EVENT and clk = '1' then
-			countOut <= countOut + 1;
+		else
+			case w is
+				when '1' =>
+					if countOut(top downto top - size - 1) = to_unsigned(2 ** size - 1, size) then
+						w <= '0';
+						countIn <= (others => '0');
+					elsif clk'EVENT and clk = '1' then
+						countOut <= countOut + 1;
+					end if;
+					
+				when '0' =>
+					if countIn = to_unsigned(2 ** size - 1, size) then
+						w <= '1';
+						countOut <= (others => '0');
+					elsif r_done'EVENT and r_done = '1' then
+						countIn <= countIn + 1;
+					end if;
+				
+				when others => report "unreachable" severity failure;
+			end case;
 		end if;
 	end process count;
 	
